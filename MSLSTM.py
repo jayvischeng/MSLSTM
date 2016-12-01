afbd50722520ddcb593b3082e6872d21d3c5f5c4
designer357 <designer357@gmail.com>
2016-12-01 21:04:12 +0800

    add inspection profiles

--- MSLSTM.py
+++ MSLSTM.py
@@ -1,12 +1,1 @@
-import tensorflow as tf
-def sess_init():
-    init = tf.initialize_all_tables()
-    config = tf.ConfigProto()
-    config.gpu_option.allow_groth = True
-    sess = tf.Session(config=config)
-    sess.run(init)
-    return sess
-
-def train():
-    with tf.Graph().as_default():
-        glo
+from __future__ import division
