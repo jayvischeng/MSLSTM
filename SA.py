8c92f21bc292b0708c932c1f52aac92b795523d3
designer357 <designer357@gmail.com>
2016-12-13 16:53:44 +0800

    updated visualize function

--- SA.py
+++ SA.py
@@ -2,7 +2,7 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib
 def set_style():
-    plt.style.use(['classic'])
+    plt.style.use(['seaborn-paper'])
     matplotlib.rc("font", family="serif")
 set_style()
 def Plotting(filename,subtitle,method):
