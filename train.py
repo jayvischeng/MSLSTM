40380c09add5d25680f4a2b799b90c848185dd21
designer357 <cityumc@hotmail.com>
2017-01-09 22:11:09 +0800

    updated mslstm

--- train.py
+++ train.py
@@ -21,9 +21,10 @@ flags = tf.app.flags
 flags.DEFINE_string('data_dir',os.path.join(os.getcwd(),'BGP_Data'),"""Directory for storing BGP_Data set""")
 flags.DEFINE_string('is_multi_scale',True,"""Run with multi-scale or not""")
 flags.DEFINE_string('input_dim',33,"""Input dimension size""")
-flags.DEFINE_string('num_neurons1',150,"""Number of hidden units""")
-flags.DEFINE_string('num_neurons2',100,"""Number of hidden units""")
+flags.DEFINE_string('num_neurons1',100,"""Number of hidden units""")
+flags.DEFINE_string('num_neurons2',16,"""Number of hidden units""")
 flags.DEFINE_string('sequence_window',20,"""Sequence window size""")
+flags.DEFINE_string('attention_size',10,"""attention size""")
 flags.DEFINE_string('scale_levels',10,"""Scale level value""")
 flags.DEFINE_string('number_class',2,"""Number of output nodes""")
 flags.DEFINE_string('wave_type','db1',"""Type of wavelet""")
@@ -39,12 +40,25 @@ flags.DEFINE_string('output','./output/',"""Directory where to write the results
 
 FLAGS = flags.FLAGS
 
-
-def pprint(msg):
+def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
+    assert inputs.shape[0] == targets.shape[0]
+    if shuffle:
+        indices = np.arange(inputs.shape[0])
+        np.random.shuffle(indices)
+    for start_idx in range(0, inputs.shape[0] - batchsize + 1, batchsize):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batchsize]
+        else:
+            excerpt = slice(start_idx, start_idx + batchsize)
+        yield inputs[excerpt], targets[excerpt]
+def pprint(msg,method=''):
     if not 'Warning' in msg:
-        sys.stdout = printlog.PyLogger('')
+        sys.stdout = printlog.PyLogger('',method)
         print(msg)
-        sys.stderr.write(msg+'\n')
+        try:
+            sys.stderr.write(msg+'\n')
+        except:
+            pass
 
 
         #sys.stdout.flush()
@@ -53,7 +67,7 @@ def sess_run(commander,data,label):
     return sess.run(commander, {data_x: data, data_y: label})
 
 def train_lstm(method,filename,cross_cv,tab_cross_cv,result_list_dict,evaluation_list):
-    global sess, data_x, data_y
+    global sess, data_x, data_y, tempstdout
     FLAGS.option = method
 
     x_train, y_train, x_test, y_test = loaddata.GetData(FLAGS.pooling_type, FLAGS.is_add_noise, FLAGS.noise_ratio, 'Attention', FLAGS.data_dir,
@@ -99,20 +113,27 @@ def train_lstm(method,filename,cross_cv,tab_cross_cv,result_list_dict,evaluation
             #else:
                 #break
             ptr = 0
+            for j_batch in iterate_minibatches(x_train,y_train,FLAGS.batch_size,shuffle=True):
+                inp, out = j_batch
+                sess_run(minimize,inp,out)
+                training_acc, training_loss = sess_run((accuracy, loss), inp, out)
+                #sys.stdout = tempstdout
 
-            for j in range(no_of_batches):
+                val_acc, val_loss = sess_run((accuracy, loss), x_test, y_test)
+            pprint(
+                FLAGS.option + "_Epoch%s" % (str(i + 1)) + ">" * 20 + "=" + "train_accuracy: %s, train_loss: %s" % (
+                str(training_acc), str(training_loss)) \
+                + ",\tval_accuracy: %s, val_loss: %s" % (str(val_acc), str(val_loss)), method + '_' + filename)
+            #for j in range(no_of_batches):
                 #pprint(str(i+1)+'th epoches,'+str(j+1)+'th batches')
-                inp, out = x_train[ptr:ptr + FLAGS.batch_size], y_train[ptr:ptr + FLAGS.batch_size]
-                inp2, out2 = x_test[ptr:ptr + FLAGS.batch_size], y_test[ptr:ptr + FLAGS.batch_size]
-
+                #inp, out = x_train[ptr:ptr + FLAGS.batch_size], y_train[ptr:ptr + FLAGS.batch_size]
+                #inp2, out2 = x_test[ptr:ptr + FLAGS.batch_size], y_test[ptr:ptr + FLAGS.batch_size]
                 #inp, out = x_train[:, ptr:ptr + FLAGS.batch_size], y_train[ptr:ptr + FLAGS.batch_size]
                 #inp2, out2 = x_test[:, ptr:ptr + FLAGS.batch_size], y_test[ptr:ptr + FLAGS.batch_size]
-
-                ptr += FLAGS.batch_size
-                sess_run(minimize,inp,out)
-                training_acc, training_loss = sess_run((accuracy,loss),inp,out)
-                val_acc, val_loss = sess_run((accuracy,loss),inp2,out2)
-
+                #ptr += FLAGS.batch_size
+                #sess_run(minimize,inp,out)
+                #training_acc, training_loss = sess_run((accuracy,loss),inp,out)
+                #val_acc, val_loss = sess_run((accuracy,loss),inp2,out2)
                 #if j%5 == 0:
                     #summary_str = sess.run(summary_op, {data_x: inp, data_y: out})
                     #summary_writer.add_summary(summary_str, i * no_of_batches)
@@ -122,8 +143,8 @@ def train_lstm(method,filename,cross_cv,tab_cross_cv,result_list_dict,evaluation
             epoch_val_loss_list.append(val_loss)
             epoch_val_acc_list.append(val_acc)
 
-            pprint(FLAGS.option+"_Epoch%s" % (str(i + 1)) + ">" * 20 + "=" + "train_accuracy: %s, train_loss: %s" % (str(training_acc), str(training_loss)) \
-                  + ",\tval_accuracy: %s, val_loss: %s" % (str(val_acc), str(val_loss)))
+            #pprint(FLAGS.option+"_Epoch%s" % (str(i + 1)) + ">" * 20 + "=" + "train_accuracy: %s, train_loss: %s" % (str(training_acc), str(training_loss)) \
+                  #+ ",\tval_accuracy: %s, val_loss: %s" % (str(val_acc), str(val_loss)),method+'_'+filename)
 
             try:
                 max_val_acc = epoch_val_acc_list[-2]
@@ -202,13 +223,13 @@ def train(method,filename,cross_cv,tab_cross_cv,wave_type='db1'):
         if 'L' in method:
             sys.stdout = tempstdout
             if method == '1L' or method == '2L':
-                FLAGS.learning_rate = 0.05
+                FLAGS.learning_rate = 0.04
                 FLAGS.is_multi_scale = False
             elif 'AL' == method:
-                FLAGS.learning_rate = 0.02
+                FLAGS.learning_rate = 0.04
                 FLAGS.is_multi_scale = False
             else:
-                FLAGS.learning_rate = 0.05
+                FLAGS.learning_rate = 0.1
                 FLAGS.is_multi_scale = True
                 FLAGS.wave_type = wave_type
             return train_lstm(method,filename,cross_cv,tab_cross_cv,result_list_dict,evaluation_list)
@@ -232,7 +253,7 @@ def main(unused_argv):
 
     #case = ['1L','2L','AL','HL','HAL']
     #case = ['1L','AL']
-    case = ['1L','HL']
+    case = ['HAL']
     #case = ['HL','AL','HAL']
     #case = ["SVM","SVMF","SVMW","NB","NBF","NBW","DT","Ada.Boost"]
     # method_list2 = ["MLP","RNN","LSTM"]
@@ -240,7 +261,7 @@ def main(unused_argv):
     case_label = {'1L':'LSTM','2L':'2-LSTM','AL':'ALSTM','HL':'HLSTM','HAL':'HALSTM'}
 
     cross_cv = 2
-    tab_cross_cv = 1
+    tab_cross_cv = 0
     wave_type = wave_type_list[0]
 
     for filename in filename_list:
