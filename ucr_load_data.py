c91e99628b90a2b7ee5ab8c56d35ab8eef69470a
designer357 <designer357@gmail.com>
2017-02-20 23:58:53 +0800

    add save function to output weights

--- ucr_load_data.py
+++ ucr_load_data.py
@@ -80,6 +80,7 @@ def load_ucr_data(is_multi_scale=False,filename='wafer'):
                 testY.append(int(val[0]))
                 testX.append(list(map(lambda a:float(a),val[1:])))
         count += 1
+
     trainX = np.array(trainX)
     trainY = np.array(trainY)
     testX = np.array(testX)
@@ -126,8 +127,8 @@ def load_ucr_data(is_multi_scale=False,filename='wafer'):
     #trainX2 = np.reshape(trainX2,(len(trainX2),len(trainX2[0]),1))
     #testX2 = np.reshape(testX2,(len(testX2),len(testX2[0]),1))
 
-    trainX = np.reshape(trainX,(len(trainX),len(trainX[0]),1))
-    testX = np.reshape(testX,(len(testX),len(testX[0]),1))
+    ####trainX = np.reshape(trainX,(len(trainX),len(trainX[0]),1))
+    ####testX = np.reshape(testX,(len(testX),len(testX[0]),1))
 
     return trainX,trainY,testX,testY
 
