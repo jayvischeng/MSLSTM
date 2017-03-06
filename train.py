e175db72fc7e1605d2226e90519426303822b685
designer357 <designer357@gmail.com>
2017-03-06 23:46:58 +0800

    add save function to output weights

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ flags.DEFINE_string('max_grad_norm',5,"""Maximum gradient norm during training""
 flags.DEFINE_string('wave_type','haar',"""Type of wavelet""")
 flags.DEFINE_string('pooling_type','max pooling',"""Type of wavelet""")
 flags.DEFINE_string('batch_size',1000,"""Batch size""")
-flags.DEFINE_string('max_epochs',200,"""Number of epochs to run""")
+flags.DEFINE_string('max_epochs',100,"""Number of epochs to run""")
 flags.DEFINE_string('learning_rate',0.01,"""Learning rate""")
 flags.DEFINE_string('is_add_noise',False,"""Whether add noise""")
 flags.DEFINE_string('noise_ratio',0,"""Noise ratio""")
@@ -263,7 +263,7 @@ def main(unused_argv):
 
     case_label = {'1L':'LSTM','2L':'2-LSTM','3L':'3-LSTM','AL':'ALSTM','HL':'HLSTM','HAL':'HALSTM'}
     #case = ['1L','2L','AL','HL','HAL']
-    case = ['2L']
+    case = ['HAL']
     #case = ["SVM","SVMF","SVMW","NB","NBF","NBW","DT","Ada.Boost"]
     #case = ["MLP","RNN","LSTM"]
 
