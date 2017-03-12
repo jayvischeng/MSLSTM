4eabdd2b6c32c45f79194b4e0789662a96e56828
designer357 <designer357@gmail.com>
2017-03-12 17:20:14 +0800

    update baseline

--- baselines/nnkeras.py
+++ baselines/nnkeras.py
@@ -3,6 +3,8 @@ import evaluation
 from sklearn.feature_selection import RFE
 from collections import defaultdict
 import numpy as np
+np.random.seed(1337)  # for reproducibility
+import keras
 from numpy import *
 from sklearn import tree
 from keras.models import Sequential
@@ -35,43 +37,34 @@ def pprint(msg,method=''):
 def Basemodel(_model,filename,cross_cv,tab_crosscv):
 
     result_list_dict = defaultdict(list)
-    evaluation_list = ["ACCURACY", "F1_SCORE", "AUC", "G_MEAN"]
+    evaluation_list = ["AUC", "G_MEAN","ACCURACY","F1_SCORE"]
     for each in evaluation_list:
         result_list_dict[each] = []
-    np.random.seed(1337)  # for reproducibility
     # num_selected_features = 25#AS leak tab=0
     # num_selected_features = 32#Slammer tab=0
     num_selected_features = 33  # Nimda tab=1
-    for tab_cv in range(cross_cv):
+    x_train, y_train, x_val, y_val, x_test, y_test = loaddata.get_data_withoutS(FLAGS.pooling_type, FLAGS.is_add_noise, FLAGS.noise_ratio, FLAGS.data_dir,
+                                            filename, FLAGS.sequence_window, tab_crosscv, cross_cv,
+                                            multiScale=False, waveScale=FLAGS.scale_levels,
+                                            waveType=FLAGS.wave_type)
+
+    FLAGS.sequence_window = x_train.shape[1]
+    FLAGS.input_dim = x_train.shape[-1]
+    FLAGS.number_class = y_train.shape[1]
+    FLAGS.batch_size = int(y_train.shape[0])
+
+    for tab_cv in range(2):
         if tab_cv == tab_crosscv: continue
-        np.random.seed(1337)  # for reproducibility
         # using MLP to train
         if _model == "MLP":
-            x_train, y_train, y_train0, x_test, y_test, y_test0 = loaddata.GetData_WithoutS(FLAGS.is_add_noise, FLAGS.noise_ratio,
-                                                                                            FLAGS.data_dir, filename,
-                                                                                            FLAGS.sequence_window, tab_crosscv,
-                                                                                            cross_cv,
-                                                                                            Multi_Scale=FLAGS.is_multi_scale,
-                                                                                            Wave_Let_Scale=FLAGS.scale_levels,
-                                                                                            Normalize=0)
-            #x_train, y_train, x_test, y_test = ucr_load_data.load_ucr_data(FLAGS.is_multi_scale,filename)
-            FLAGS.sequence_window = x_train.shape[len(x_train.shape) - 2]
-            # FLAGS.sequence_window = 900
-            FLAGS.input_dim = x_train.shape[-1]
-            FLAGS.number_class = y_train.shape[1]
-            FLAGS.batch_size = int(y_train.shape[0] / 2)
-            if FLAGS.is_multi_scale:
-                FLAGS.scale_levels = x_train.shape[1]
-                FLAGS.sequence_window = x_train.shape[len(x_train.shape) - 2]
-                FLAGS.input_dim = x_train.shape[-1]
-                FLAGS.number_class = y_train.shape[1]
-                FLAGS.batch_size = int(y_train.shape[0] / 2)
-            else:
-                FLAGS.sequence_window = x_train.shape[1]
-                FLAGS.input_dim = x_train.shape[-1]
-                FLAGS.number_class = y_train.shape[1]
-                FLAGS.batch_size = int(y_train.shape[0])
-            #FLAGS.sequence_window = 270
+            #x_train, y_train, y_train0, x_test, y_test, y_test0 = loaddata.GetData_WithoutS(FLAGS.is_add_noise, FLAGS.noise_ratio,
+            #                                                                                FLAGS.data_dir, filename,
+            #                                                                                FLAGS.sequence_window, tab_crosscv,
+            #                                                                                cross_cv,
+            #                                                                                Multi_Scale=FLAGS.is_multi_scale,
+            #                                                                                Wave_Let_Scale=FLAGS.scale_levels,
+            #                                                                                Normalize=0)
+
             pprint(_model + " is running..............................................")
             start = time.clock()
             model = Sequential()
@@ -82,7 +75,7 @@ def Basemodel(_model,filename,cross_cv,tab_crosscv):
             # model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
             model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-            model.fit(x_train, y_train, validation_data=(x_test, y_test), batch_size=FLAGS.batch_size, nb_epoch=FLAGS.max_epochs)
+            model.fit(x_train, y_train, validation_data=(x_val, y_val), batch_size=FLAGS.batch_size, nb_epoch=FLAGS.max_epochs)
             # result = model.predict(X_Testing, batch_size=FLAGS.batch_size)
             result = model.predict(x_test)
             end = time.clock()
@@ -91,25 +84,22 @@ def Basemodel(_model,filename,cross_cv,tab_crosscv):
         elif _model == "RNN":
             pprint(_model + " is running..............................................")
             start = time.clock()
-            x_train, y_train, x_test, y_test = loaddata.GetData(FLAGS.pooling_type,FLAGS.is_add_noise,
-                                                                FLAGS.noise_ratio,
-                                                                'Attention',
-                                                                FLAGS.data_dir,
-                                                                filename,
-                                                                FLAGS.sequence_window,
-                                                                tab_crosscv,
-                                                                cross_cv,
-                                                                Multi_Scale=False,
-                                                                Wave_Let_Scale=FLAGS.scale_levels)
+            x_train, y_train, x_val, y_val, x_test, y_test = loaddata.get_data(FLAGS.pooling_type, FLAGS.is_add_noise,
+                                                                               FLAGS.noise_ratio, FLAGS.data_dir,
+                                                                               filename, FLAGS.sequence_window,
+                                                                               tab_crosscv, cross_cv,
+                                                                               multiScale=False,
+                                                                               waveScale=FLAGS.scale_levels,
+                                                                               waveType=FLAGS.wave_type)
 
             rnn_object = SimpleRNN(FLAGS.num_neurons1, input_length=len(x_train[0]), input_dim=FLAGS.input_dim)
             model = Sequential()
             model.add(rnn_object)  # X.shape is (samples, timesteps, dimension)
-            model.add(Dense(30, activation="sigmoid"))
+            #model.add(Dense(30, activation="sigmoid"))
             model.add(Dense(output_dim=FLAGS.number_class))
             model.add(Activation("sigmoid"))
-            model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-            model.fit(x_train, y_train, validation_data=(x_test, y_test), batch_size=FLAGS.batch_size, nb_epoch=FLAGS.max_epochs)
+            model.compile(optimizer='adam', learning_rate=0.01, loss='binary_crossentropy', metrics=['accuracy'])
+            model.fit(x_train, y_train, validation_data=(x_val, y_val), batch_size=FLAGS.batch_size, nb_epoch=FLAGS.max_epochs)
             result = model.predict(x_test)
             end = time.clock()
             pprint("The Time For RNN is " + str(end - start))
@@ -118,45 +108,23 @@ def Basemodel(_model,filename,cross_cv,tab_crosscv):
         elif _model == "LSTM":
             pprint(_model + " is running..............................................")
             start = time.clock()
-            x_train, y_train, x_test, y_test = loaddata.GetData(FLAGS.pooling_type,FLAGS.is_add_noise,
-                                                                FLAGS.noise_ratio,
-                                                                'Attention',
-                                                               FLAGS.data_dir,
-                                                                filename,
-                                                                FLAGS.sequence_window,
-                                                                tab_crosscv,
-                                                                cross_cv,
-                                                                Multi_Scale=False,
-                                                                Wave_Let_Scale=FLAGS.scale_levels)
+            x_train, y_train, x_val, y_val, x_test, y_test = loaddata.get_data(FLAGS.pooling_type, FLAGS.is_add_noise,
+                                                                               FLAGS.noise_ratio, FLAGS.data_dir,
+                                                                               filename, FLAGS.sequence_window,
+                                                                               tab_crosscv, cross_cv,
+                                                                               multiScale=False,
+                                                                               waveScale=FLAGS.scale_levels,
+                                                                               waveType=FLAGS.wave_type)
 
-            #x_train, y_train, x_test, y_test = ucr_load_data.load_ucr_data(FLAGS.is_multi_scale,filename)
-            FLAGS.sequence_window = x_train.shape[len(x_train.shape) - 2]
-            FLAGS.sequence_window = 90
-            FLAGS.input_dim = x_train.shape[-1]
-            FLAGS.number_class = y_train.shape[1]
-            FLAGS.batch_size = int(y_train.shape[0] / 2)
-            if FLAGS.is_multi_scale:
-                FLAGS.scale_levels = x_train.shape[1]
-                FLAGS.sequence_window = x_train.shape[len(x_train.shape) - 2]
-                FLAGS.input_dim = x_train.shape[-1]
-                FLAGS.number_class = y_train.shape[1]
-                FLAGS.batch_size = int(y_train.shape[0] / 2)
-            else:
-                FLAGS.sequence_window = x_train.shape[1]
-                FLAGS.input_dim = x_train.shape[-1]
-                FLAGS.number_class = y_train.shape[1]
-                FLAGS.batch_size = int(y_train.shape[0])
             lstm_object = LSTM(FLAGS.num_neurons1, input_length=len(x_train[0]), input_dim=FLAGS.input_dim)
             model = Sequential()
             model.add(lstm_object)  # X.shape is (samples, timesteps, dimension)
-            model.add(Activation('tanh'))
             #model.add(Dense(30, activation="relu"))
             model.add(Dense(output_dim=FLAGS.number_class))
             model.add(Activation("softmax"))
-            #model.compile(optimizer='adam', learning_rate=FLAGS.learning_rate,loss='categorical_crossentropy', metrics=['accuracy'])
-            model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['accuracy'])
-
-            model.fit(x_train, y_train,validation_data=(x_test, y_test), batch_size=FLAGS.batch_size, nb_epoch=FLAGS.max_epochs)
+            sgd = keras.optimizers.SGD(lr=0.02, momentum=0.0, decay=0.0, nesterov=False)
+            model.compile(optimizer='sgd',loss='binary_crossentropy', metrics=['accuracy'])
+            model.fit(x_train, y_train,validation_data=(x_val, y_val), batch_size=FLAGS.batch_size, nb_epoch=FLAGS.max_epochs)
             result = model.predict(x_test)
             end = time.clock()
             pprint("The Time For LSTM is " + str(end - start))
@@ -173,17 +141,20 @@ def Basemodel(_model,filename,cross_cv,tab_crosscv):
                 for tab in range(len(result2)):
                     fout.write(str(int(result2[tab])) + '\n')
 
-        for each_eval, each_result in results.items():
-            result_list_dict[each_eval].append(each_result)
-
-    for eachk, eachv in result_list_dict.items():
-        result_list_dict[eachk] = np.average(eachv)
+        #for each_eval, each_result in results.items():
+            #result_list_dict[each_eval].append(each_result)
+        for each_eval in evaluation_list:
+            result_list_dict[each_eval].append(results[each_eval])
+    #for eachk, eachv in result_list_dict.items():
+        #result_list_dict[eachk] = np.average(eachv)
     if FLAGS.is_add_noise == False:
         with open(os.path.join(FLAGS.output, "Comparison_Log_" + filename), "a")as fout:
             outfileline = _model + ":__"
             fout.write(outfileline)
-            for eachk, eachv in result_list_dict.items():
-                fout.write(eachk + ": " + str(round(eachv, 3)) + ",\t")
+            for each_eval in evaluation_list:
+                fout.write(each_eval + ": " + str(round(np.average(result_list_dict[each_eval]), 3)) + ",\t")
+            #for eachk, eachv in result_list_dict.items():
+                #fout.write(eachk + ": " + str(round(eachv, 3)) + ",\t")
             fout.write('\n')
 
     return results
