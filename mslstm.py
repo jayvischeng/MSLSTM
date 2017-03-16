ec94fa44ea3202a487df549cd6b4f40a23b8f014
designer357 <cityumc@hotmail.com>
2017-03-16 21:32:58 +0800

    add save function to output weights

--- mslstm.py
+++ mslstm.py
@@ -71,8 +71,8 @@ def last_relevant(output, length):
     return relevant
 def loss_(predict,label):
 
-    cost_cross_entropy = -tf.reduce_mean(label * tf.log(predict))
-    #cost_cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(predict, label, name=None)  # Sigmoid
+    #cost_cross_entropy = -tf.reduce_mean(label * tf.log(predict))
+    cost_cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(label,predict))  # Sigmoid
 
     #cost_cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(predict, label, name=None))  # Sigmoid
     #cost_cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(label,predict))  # Sigmoid
@@ -99,19 +99,39 @@ def loss_(predict,label):
 def print_info(tensor,name):
     return tf.Print(tensor, [tensor.get_shape()], "The "+name+" shape is :", first_n=4096, summarize=40)
 
-def inference(data,label,option,is_training):
-    if option == '1L':#pure one-layer lstm
-        #lstm_cell = BNLSTMCell(FLAGS.num_neurons1,is_training)
+def weight_bias():
+    weights = {
+        'in': tf.Variable(tf.random_normal([FLAGS.input_dim, FLAGS.num_neurons1])),
+        'out_layer1': tf.Variable(tf.random_normal([FLAGS.num_neurons1, FLAGS.number_class])),
+        'out_layer2': tf.Variable(tf.random_normal([FLAGS.num_neurons2, FLAGS.number_class]))
+    }
+    biases = {
+        'in': tf.Variable(tf.constant(0.1, shape=[FLAGS.num_neurons1, ])),
+        'out': tf.Variable(tf.constant(0.1, shape=[FLAGS.number_class, ]))
 
+    }
+    return weights, biases
+def inference(data,label,option,is_training):
+    weights,biases = weight_bias()
+    if option == '1L':#pure one-layer lstm
+        X = tf.reshape(data, [-1, FLAGS.input_dim])
+        # X_in = W*X + b
+        X_in = tf.matmul(X, weights['in']) + biases['in']
+        # X_in ==> ( batches,  steps,  hidden) 换回3维
+        X_in = tf.reshape(X_in, [-1, FLAGS.sequence_window, FLAGS.num_neurons1])
+        #lstm_cell = BNLSTMCell(FLAGS.num_neurons1,is_training)
         lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(FLAGS.num_neurons1, forget_bias=1.0, activation=tf.nn.tanh)
         #lstm_cell = tf.contrib.rnn.BasicRNNCell(FLAGS.num_neurons1, activation=tf.nn.tanh)
         #lstm_cell = tf.contrib.rnn_cell.MultiRNNCell([lstm_cell]*2)
-        val, state = tf.nn.dynamic_rnn(lstm_cell, data, dtype=tf.float32)
+        val, state = tf.nn.dynamic_rnn(lstm_cell, X_in, dtype=tf.float32)
         val = tf.transpose(val, [1, 0, 2])
         last = tf.gather(val, int(val.get_shape()[0]) - 1)
-        weight = tf.Variable(tf.truncated_normal([FLAGS.num_neurons1, int(label.get_shape()[1])]),name='weight')
-        bias = tf.Variable(tf.constant(0.1, shape=[label.get_shape()[1]]))
-        prediction = tf.nn.sigmoid(tf.matmul(last, weight) + bias)
+        #last = tf.unpack(tf.transpose(val, [1, 0, 2]))
+        #weight = tf.Variable(tf.truncated_normal([FLAGS.num_neurons1, int(label.get_shape()[1])]),name='weight')
+        #bias = tf.Variable(tf.constant(0.1, shape=[label.get_shape()[1]]))
+        prediction = tf.nn.sigmoid(tf.matmul(last, weights['out_layer1']) + biases['out'])
+        #prediction = tf.matmul(last, weights['out_layer1']) + biases['out']
+
     elif option == 'RNN':#pure one-layer RNN
         #lstm_cell = BNLSTMCell(FLAGS.num_neurons1,is_training)
 
