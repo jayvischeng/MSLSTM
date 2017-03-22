d5c54515568a1df3c6fe7b2f35d529082a6842ad
designer357 <designer357@gmail.com>
2017-03-22 22:47:37 +0800

    .

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ flags = tf.app.flags
 flags.DEFINE_string('data_dir',os.path.join(os.getcwd(),'BGP_Data'),"""Directory for storing BGP_Data set""")
 flags.DEFINE_string('is_multi_scale',False,"""Run with multi-scale or not""")
 flags.DEFINE_string('input_dim',33,"""Input dimension size""")
-flags.DEFINE_string('num_neurons1',128,"""Number of hidden units""")#HAL(hn1=32,hn2=16)
+flags.DEFINE_string('num_neurons1',64,"""Number of hidden units""")#HAL(hn1=32,hn2=16)
 flags.DEFINE_string('num_neurons2',20,"""Number of hidden units""")#16,32
 flags.DEFINE_string('sequence_window',30,"""Sequence window size""")
 flags.DEFINE_string('attention_size',10,"""attention size""")
@@ -14,7 +14,7 @@ flags.DEFINE_string('max_grad_norm',5,"""Maximum gradient norm during training""
 flags.DEFINE_string('wave_type','haar',"""Type of wavelet""")
 flags.DEFINE_string('pooling_type','max pooling',"""Type of wavelet""")
 flags.DEFINE_string('batch_size',200,"""Batch size""")
-flags.DEFINE_string('max_epochs',200,"""Number of epochs to run""")
+flags.DEFINE_string('max_epochs',100,"""Number of epochs to run""")
 flags.DEFINE_string('learning_rate',0.01,"""Learning rate""")
 flags.DEFINE_string('is_add_noise',False,"""Whether add noise""")
 flags.DEFINE_string('noise_ratio',0,"""Noise ratio""")
