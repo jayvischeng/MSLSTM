6a185eaeb5d186ea60376ee4ea3c7168588d2833
designer357 <designer357@gmail.com>
2017-03-29 15:30:13 +0800

    update evaulation

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve,auc
+import sklearn
+import loaddata
 def ComputeAUC(y_test,y_predict):
     fpr = dict()
     tpr = dict()
@@ -18,14 +20,70 @@ def ComputeAUC(y_test,y_predict):
 
 
 
-def ReverseEncoder(y):
-    temp = []
-    for tab1 in range(len(y)):
-            temp.append(list(y[tab1]).index(max(list(y[tab1]))))
-    return temp
-
 
 def evaluation(label,predict,trigger_flag,evalua_flag,positive_label=1,negative_label=0):
+
+    try:
+        label_ = loaddata.reverse_one_hot(label)
+        predict_ = loaddata.reverse_one_hot(predict)
+    except:
+        label_ = list(label)
+        predict_ = list(predict)
+
+    ac_positive = 0
+    ac_negative = 0
+    correct = 0
+    error_rate_flag = False
+
+
+    #output class error rate
+    predict_positive = predict_.count(positive_label)
+    total_positive = label_.count(positive_label)
+
+
+    predict_negative = predict_.count(negative_label)
+    total_negative = label_.count(negative_label)
+    print("TP is "+str(total_positive))
+    print("TN is "+str(total_negative))
+
+    for tab_sample in range(len(predict_)):
+        if predict_[tab_sample] == label_[tab_sample]:
+            correct += 1
+            if int(label_[tab_sample]) == positive_label:
+                ac_positive += 1
+            elif int(label_[tab_sample]) == negative_label:
+                ac_negative += 1
+
+
+    if evalua_flag == True:
+        try:
+            ACC_A = float(ac_positive) / predict_positive
+        except:
+            ACC_A = float(ac_positive) * 100 / (predict_positive + 1)
+
+        AUC = roc_auc_score(label_, np.transpose(np.array(predict_)))
+        G_MEAN = np.sqrt(float(ac_positive * ac_negative) / (total_negative * total_positive))
+
+        PRECISION = ACC_A
+        RECALL = float(ac_positive) / total_positive
+        ACCURACY = round(float(ac_positive + ac_negative) / len(label_), 5)
+        #F1_SCORE = sklearn.metrics.f1_score(label_, predict_)
+        try:
+            F1_SCORE = round((2 * PRECISION * RECALL) / (PRECISION + RECALL), 5)
+        except:
+            F1_SCORE = 0.01 * round((2 * PRECISION * RECALL) / (PRECISION + RECALL + 1), 5)
+
+        return {"ACCURACY": ACCURACY, "F1_SCORE": F1_SCORE, "AUC": AUC, "G_MEAN": G_MEAN}
+
+
+    else:
+        AUC = roc_auc_score(label_, np.transpose(np.array(predict_)))
+        G_MEAN = np.sqrt(float(ac_positive * ac_negative) / (total_negative * total_positive))
+        FPR,TPR,AUC = ComputeAUC(label_,predict_)
+        print({"FPR":FPR,"TPR":TPR,"AUC":AUC,"G_MEAN":G_MEAN})
+        return {"FPR":FPR,"TPR":TPR,"AUC":AUC,"G_MEAN":G_MEAN}
+        #return fpr,tpr,auc#evaluate auc
+def evaluation2(label,predict,trigger_flag,evalua_flag,positive_label=1,negative_label=0):
 
     if trigger_flag or evalua_flag == False:
         Output_Class = [[] for i in range(len(label[0]))]
@@ -96,6 +154,7 @@ def evaluation(label,predict,trigger_flag,evalua_flag,positive_label=1,negative_
         PRECISION = ACC_A
         RECALL = float(ac_positive) / total_positive
         ACCURACY = round(float(ac_positive + ac_negative) / len(label), 5)
+        #F1_SCORE = sklearn.metrics.f1_score(label, Output_Class)
         try:
             F1_SCORE = round((2 * PRECISION * RECALL) / (PRECISION + RECALL), 5)
         except:
